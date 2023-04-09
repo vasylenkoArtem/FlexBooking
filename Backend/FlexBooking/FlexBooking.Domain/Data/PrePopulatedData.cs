02d70add390efec50cda0ab2bb5961bf20d93423
Vladyslav <vladislav.sheyko@hotmail.com>
2023-04-09 16:18:36 -0400

    Added PrePopulated data

--- Backend/FlexBooking/FlexBooking.Domain/Data/PrePopulatedData.cs
+++ Backend/FlexBooking/FlexBooking.Domain/Data/PrePopulatedData.cs
@@ -15,60 +15,60 @@ public static class PrePopulatedData
             new() { Id = 2, Username = "TestAdmin", Password = "123456", RoleId = UserRoles.Admin }
         });
 
-        // modelBuilder.Entity<OfferLocation>().HasData(new List<OfferLocation>()
-        // {
-        //     new() {Id = 1, City = "Toronto", AirportCode = "YYZ"},
-        //     new() {Id = 2, City = "Montreal", AirportCode = "YUL"},
-        //     new() {Id = 3, City = "Toronto", TrainStation = "Union Station"},
-        //     new() {Id = 4, City = "Montreal", TrainStation = "Central Station"},
-        //     new() {Id = 5, City = "Toronto", BusStation = "Toronto Bus Station"},
-        //     new() {Id = 5, City = "Montreal", BusStation = "Montreal Bus Station"},
-        // });
-        //
-        // modelBuilder.Entity<BookingOffer>().HasData(new List<BookingOffer>()
-        // {
-        //     new()
-        //     {
-        //         Id = 1,
-        //         OriginOfferLocationId = 1,
-        //         DestinationOfferLocationId = 2,
-        //         DepartureDateUtc = DateTime.UtcNow,
-        //         ArrivalDateUtc = DateTime.UtcNow.Add(TimeSpan.FromHours(6)), 
-        //         AvailablePassengerSeats = 100,
-        //         OfferTypeId = OfferType.Flight
-        //     },
-        //     new()
-        //     {
-        //         Id = 2,
-        //         OriginOfferLocationId = 3,
-        //         DestinationOfferLocationId = 4,
-        //         DepartureDateUtc = DateTime.UtcNow,
-        //         ArrivalDateUtc = DateTime.UtcNow.Add(TimeSpan.FromHours(6)), 
-        //         AvailablePassengerSeats = 100,
-        //         OfferTypeId = OfferType.Train
-        //     },
-        //     new()
-        //     {
-        //         Id = 3,
-        //         OriginOfferLocationId = 5,
-        //         DestinationOfferLocationId = 6,
-        //         DepartureDateUtc = DateTime.UtcNow,
-        //         ArrivalDateUtc = DateTime.UtcNow.Add(TimeSpan.FromHours(6)), 
-        //         AvailablePassengerSeats = 100,
-        //         OfferTypeId = OfferType.Bus
-        //     }
-        // });
-        //
-        // modelBuilder.Entity<CarOffer>().HasData(new List<CarOffer>()
-        // {
-        //     new() {Id = 1, City = "Toronto", CarImageUrl = "https://www.torontoairportlimousine.com/wp-content/uploads/2019/03/limo-toronto-airport.jpg", Price = 100},
-        //     new() {Id = 2, City = "Montreal", CarImageUrl = "https://www.torontoairportlimousine.com/wp-content/uploads/2019/03/limo-toronto-airport.jpg", Price = 100}
-        // });
-        //
-        // modelBuilder.Entity<HotelOffer>().HasData(new List<HotelOffer>()
-        // {
-        //     new() {Id = 1, City = "Toronto", HotelRoomImageUrl = "https://www.torontoairportlimousine.com/wp-content/uploads/2019/03/limo-toronto-airport.jpg", Price = 100},
-        //     new() {Id = 2, City = "Montreal", HotelRoomImageUrl = "https://www.torontoairportlimousine.com/wp-content/uploads/2019/03/limo-toronto-airport.jpg", Price = 100}
-        // });
+        modelBuilder.Entity<OfferLocation>().HasData(new List<OfferLocation>()
+        {
+            new() {Id = 1, City = "Toronto", AirportCode = "YYZ"},
+            new() {Id = 2, City = "Montreal", AirportCode = "YUL"},
+            new() {Id = 3, City = "Toronto", TrainStation = "Union Station"},
+            new() {Id = 4, City = "Montreal", TrainStation = "Central Station"},
+            new() {Id = 5, City = "Toronto", BusStation = "Toronto Bus Station"},
+            new() {Id = 6, City = "Montreal", BusStation = "Montreal Bus Station"},
+        });
+        
+        modelBuilder.Entity<BookingOffer>().HasData(new List<BookingOffer>()
+        {
+            new()
+            {
+                Id = 1,
+                OriginOfferLocationId = 1,
+                DestinationOfferLocationId = 2,
+                DepartureDateUtc = DateTime.UtcNow,
+                ArrivalDateUtc = DateTime.UtcNow.Add(TimeSpan.FromHours(6)), 
+                AvailablePassengerSeats = 100,
+                OfferTypeId = OfferType.Flight
+            },
+            new()
+            {
+                Id = 2,
+                OriginOfferLocationId = 3,
+                DestinationOfferLocationId = 4,
+                DepartureDateUtc = DateTime.UtcNow,
+                ArrivalDateUtc = DateTime.UtcNow.Add(TimeSpan.FromHours(6)), 
+                AvailablePassengerSeats = 100,
+                OfferTypeId = OfferType.Train
+            },
+            new()
+            {
+                Id = 3,
+                OriginOfferLocationId = 5,
+                DestinationOfferLocationId = 6,
+                DepartureDateUtc = DateTime.UtcNow,
+                ArrivalDateUtc = DateTime.UtcNow.Add(TimeSpan.FromHours(6)), 
+                AvailablePassengerSeats = 100,
+                OfferTypeId = OfferType.Bus
+            }
+        });
+        
+        modelBuilder.Entity<CarOffer>().HasData(new List<CarOffer>()
+        {
+            new() {Id = 1, City = "Toronto", CarImageUrl = "https://www.torontoairportlimousine.com/wp-content/uploads/2019/03/limo-toronto-airport.jpg", Price = 100},
+            new() {Id = 2, City = "Montreal", CarImageUrl = "https://www.torontoairportlimousine.com/wp-content/uploads/2019/03/limo-toronto-airport.jpg", Price = 100}
+        });
+        
+        modelBuilder.Entity<HotelOffer>().HasData(new List<HotelOffer>()
+        {
+            new() {Id = 1, City = "Toronto", HotelRoomImageUrl = "https://www.torontoairportlimousine.com/wp-content/uploads/2019/03/limo-toronto-airport.jpg", Price = 100},
+            new() {Id = 2, City = "Montreal", HotelRoomImageUrl = "https://www.torontoairportlimousine.com/wp-content/uploads/2019/03/limo-toronto-airport.jpg", Price = 100}
+        });
     }
 }
