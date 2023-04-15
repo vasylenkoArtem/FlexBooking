75669e55fcc9fb40937fff7f1f9113b2c76127ab
Artem Vasylenko <artem.vasylenko@bnine.com>
2023-04-15 13:42:08 -0400

    Map Details of the Offer Short View

--- Frontend/flex-booking/src/pages/BookingOffers/BookingOffersList/FlightOfferShortView.tsx
+++ Frontend/flex-booking/src/pages/BookingOffers/BookingOffersList/FlightOfferShortView.tsx
@@ -1,44 +1,111 @@
 import { Button, Card, Col, Divider, Row } from "antd";
 import AAIcon from "../../../Icons/AAIcon.png";
-import { BookingOffer } from "./types";
+import { BookingOffer, OfferType } from "./types";
+import moment from "moment";
 
 interface PassedProps {
     bookingOffer: BookingOffer;
 }
 
+
+
+const getVehicleTypeString = (offerTypeId: OfferType) => {
+    switch (offerTypeId) {
+        case OfferType.Flight:
+            return 'Flight';
+        case OfferType.Train:
+            return 'Train';
+        case OfferType.Bus:
+            return 'Bus';
+
+        default:
+            return '';
+    }
+}
+
+const getOriginString = (bookingOffer: BookingOffer) => {
+    switch (bookingOffer.offerTypeId) {
+        case OfferType.Flight:
+            return bookingOffer.originOfferLocation.airportCode;
+        case OfferType.Train:
+            return bookingOffer.originOfferLocation.trainStation;
+        case OfferType.Bus:
+            return bookingOffer.originOfferLocation.busStation;
+
+        default:
+            return '';
+    }
+}
+
+const getDestinationString = (bookingOffer: BookingOffer) => {
+    switch (bookingOffer.offerTypeId) {
+        case OfferType.Flight:
+            return bookingOffer.destinationOfferLocation.airportCode;
+        case OfferType.Train:
+            return bookingOffer.destinationOfferLocation.trainStation;
+        case OfferType.Bus:
+            return bookingOffer.destinationOfferLocation.busStation;
+
+        default:
+            return '';
+    }
+}
+const getVehicleColor = (bofferTypeId: OfferType) => {
+    switch (bofferTypeId) {
+        case OfferType.Flight:
+            return '#0000FF';
+        case OfferType.Train:
+            return '#023020';
+        case OfferType.Bus:
+            return '#FFA500';
+
+        default:
+            return '';
+    }
+}
+
 const FlightOfferShortView = (props: PassedProps) => {
+
+    const duration = moment.duration(moment(props.bookingOffer.arrivalDateUtc).diff(moment(props.bookingOffer.departureDateUtc)));
+    const hours = duration.asHours();
+
+    const vehicleType = getVehicleTypeString(props.bookingOffer.offerTypeId);
+    const originString = getOriginString(props.bookingOffer);
+    const destinationString = getDestinationString(props.bookingOffer);
+    const vehicleColor = getVehicleColor(props.bookingOffer.offerTypeId);
+
     return <>
         <Card
             bordered={true}>
             <Row>
                 <Col span={4} style={{ padding: '8px 0' }}>
-                    <img src={AAIcon}></img>
+                    <img src={props.bookingOffer.companyLogoUrl}></img>
                 </Col>
                 <Col span={4} style={{ padding: '8px 0' }}>
-                    <b>10:55 AM</b>
+                    <b>{moment.utc(props.bookingOffer.departureDateUtc).local().format('h:mm a').toUpperCase()}</b>
 
-                    <div>{props.bookingOffer.originOfferLocation.airportCode}</div>
+                    <div>{originString}</div>
                 </Col>
 
                 <Col span={4} style={{ padding: '8px 0' }}>
                     <div style={{ textAlign: 'center' }}>
-                        1h 39m
+                        {hours}h
                         <br />
                         -------------
                         <br />
-                        <div style={{ color: '#0c838a' }}>Direct</div>
+                        <div style={{ color: vehicleColor }}>{vehicleType}</div>
                     </div>
 
                 </Col>
                 <Col span={4} style={{ padding: '8px 0' }}>
-                    <b>12:34 AM</b>
-                    <div>{props.bookingOffer.destinationOfferLocation.airportCode}</div>
+                    <b>{moment.utc(props.bookingOffer.arrivalDateUtc).local().format('h:mm a').toUpperCase()}</b>
+                    <div>{destinationString}</div>
                 </Col>
                 <Col span={1}>
                     <Divider type="vertical" style={{ height: "100%" }} />
                 </Col>
                 <Col span={6} style={{ textAlign: 'center' }}>
-                    <h1>$232</h1>
+                    <h1>${props.bookingOffer.price}</h1>
                     <Button onClick={() => window.open(`trips/${props.bookingOffer.id}`, '_blank')}>SEE DEAL</Button>
                 </Col>
             </Row>
