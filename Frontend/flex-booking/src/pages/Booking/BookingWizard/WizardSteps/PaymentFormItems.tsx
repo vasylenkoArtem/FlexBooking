4c75aeb12e6fb9195f8b9a13dcc4ec654c87a621
Artem Vasylenko <artem.vasylenko@bnine.com>
2023-05-27 11:47:38 -0400

    Disable past dates for payment card

--- Frontend/flex-booking/src/pages/Booking/BookingWizard/WizardSteps/PaymentFormItems.tsx
+++ Frontend/flex-booking/src/pages/Booking/BookingWizard/WizardSteps/PaymentFormItems.tsx
@@ -1,13 +1,17 @@
 import { DatePicker, Form, Input } from "antd";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 const Payment = () => {
     const { t } = useTranslation();
-    
+
     const getTranslation = (key: string) => {
         return t(key) as string;
     };
-    
+
+    function disabledDate(current: any) {
+        return current && current.valueOf() < Date.now();
+    }
+
     return <>
         <Form.Item
             label={t('cardNumber')}
@@ -28,9 +32,9 @@ const Payment = () => {
             name="expiryDate"
             rules={[{ required: true, message: getTranslation('invalidExpiryDateMessage') }]}
         >
-            <DatePicker placeholder={t("selectDate").toString()} />
+            <DatePicker placeholder={t("selectDate").toString()} disabledDate={disabledDate} />
         </Form.Item>
-        
+
         <Form.Item
             label={t('billingAddress')}
             name="billingAddress"
